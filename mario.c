a4b485232e7222875c2d175d3f9c5184247a622d
fg2srt4 <fg2srt4@users.noreply.github.com>
2019-03-29 16:19:47 +0000

    20190329T161924Z

--- mario.c
+++ mario.c
@@ -1,24 +1,28 @@
-#include <cs50.h>
 #include <stdio.h>
-
+#include <cs50.h>
 
 int main(void)
 {
-    
-int Height;
-do {
-    Height = get_int("How tall should it be?");
-} while (Height < 1 || Height > 8);
+   int height;
 
-for (int i = 0; i < Height; i++)
-{
-        for (int j = (Height - i); j > 0; j--){
+    // prompts user for pyramid height between 1 and 23
+    do
+    {
+        height = get_int("How tall should it be?");
+    }
+    while (height < 0 || height > 8);
+
+    // for loops specifying number of spaces and hashes per line
+    for (int line = 0; line < height; line++)
+    {
+        for (int spaces = height - line; spaces > 1; spaces--)
+        {
             printf(" ");
         }
-        for (int k = 0; k < (i + 1); k++){
-            printf("#");    
+        for (int hashes = 0; hashes < line + 1; hashes++)
+        {
+            printf("#");
         }
         printf("\n");
-        
-}
+    }
 }
